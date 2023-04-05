d4752f8832d717adb723be3be0c79a2f1ea94484
Mihai Doarna <dmihai@gmail.com>
2023-04-05 15:41:13 +0300

    retrieve input and output file names from command args

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import csv
 
 from Spreadsheet import Spreadsheet
@@ -8,8 +9,18 @@ def exportToCsv(file, data):
         writer = csv.writer(f)
         writer.writerows(data)
 
+inputFile = 'transactions.csv'
+outputFile = 'output.csv'
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--input', type=str, required=False, default=inputFile,
+                    help='Input csv file name')
+parser.add_argument('--output', type=str, required=False, default=outputFile,
+                    help='Output csv file name')
+args = parser.parse_args()
+
 data = Spreadsheet()
-data.loadFromCsv('transactions.csv')
+data.loadFromCsv(args.input)
 data.computeValues()
 values = data.getValues()
-exportToCsv('output.csv', values)
+exportToCsv(args.output, values)
